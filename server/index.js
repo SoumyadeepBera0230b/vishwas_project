4ae9ef09e58fff0cdcdca62c3b17dc896e0da356
Soumyadeep Bera <soumyadeep0230b@gmail.com>
2024-03-03 11:05:51 +0530

    First commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const Mysql = require("mysql")
+
+const cors = require('cors');
+const app = express()
+
+app.use(cors());
+
+const db = Mysql.createConnection( {
+    host: "localhost",
+    user: 'root',
+    password: 'password',
+    database: 'vishwa'
+})
+
+app.get("/api", (req, res) => {
+    const sql = "SELECT * FROM MOCK_DATA"
+    db.query(sql, (err, result) => {
+        if (!err) {
+            console.log(result)
+            return res.json(result)
+        } else {
+            return res.json(err)
+        }
+    })
+}) 
+
+app.listen(8080, ()=>{
+    console.log('Server is running on port 8080')
+});
